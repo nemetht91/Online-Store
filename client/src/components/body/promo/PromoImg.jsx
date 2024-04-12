73b8c30b8fbef2af88ee73f8e823ed990ec1bbbf
Tamas Nemeth <nemeth.t91@gmail.com>
2024-04-12 22:51:20 +0100

    add promos

--- client/src/components/body/promo/PromoImg.jsx
+++ client/src/components/body/promo/PromoImg.jsx
@@ -4,16 +4,18 @@ function PromoImg(props){
 
     const [currentImg, setCurrentImg] = useState(0);
 
-    setTimeout(() => {
-        if(props.imgs.length <= 1){
-            return;
-        }
-        if(currentImg >= props.imgs.length -1){
-            setCurrentImg(0);
-            return;
-        }
-        setCurrentImg(currentImg+1);
-    }, 5000);
+    if(props.imgs.length > 1){
+        setTimeout(() => {
+        
+            if(currentImg >= props.imgs.length -1){
+                setCurrentImg(0);
+                return;
+            }
+            setCurrentImg(currentImg+1);
+        }, 5000);
+    }
+
+    
 
     return <>
         {props.imgs.map((img, index) => {
